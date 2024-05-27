e96c8f3e19d625ed3735729d83868e1d8f429345
attrfai <attarrifaismktibazma@gmail.com>
2024-05-27 10:29:56 +0700

    Perbaikan fungsi subjectResponse

--- controllers/mapelController.js
+++ controllers/mapelController.js
@@ -25,7 +25,7 @@ async function addSubject(req, res) {
             const subjectResponse = {
                 id: newSubject.id,
                 mapel: newSubject.mapel,
-                guruPengampu: subject.guruPengampu,
+                guruPengampu: newSubject.guruPengampu,
                 createdAt: newSubject.createdAt
             };
 
