876059b44df4238ae7be84a32d8f57ca5a4943a1
Abdul-liebert <muhammad065932@smk.belajar.id>
2024-05-29 15:10:00 +0700

    Penambahan fungsi deleteSubject

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { addSubject, loginTeacher, getSubject, getAllSubjects, updateSubject } = require('../controllers/mapelController');
+const { addSubject, loginTeacher, getSubject, getAllSubjects, updateSubject, deleteSubject } = require('../controllers/mapelController');
 const authentication = require('../middleware/authentication');
 
 /* GET mapel listing. */
@@ -10,7 +10,7 @@ router.post('/login', loginTeacher);
 router.get('/get', authentication, getSubject);
 router.get('/all', authentication, getAllSubjects);
 router.put('/:id/update', authentication, updateSubject);
-
+router.delete('/:id/delete',authentication,deleteSubject);
 
 
 module.exports = router;
